471f6083e9818bb2d7deeae749fbf43e20ba7547
oryzae51 <oryzae51@gmail.com>
2020-06-18 21:58:46 +0900

    MAPq10

--- Preprocessing/MAPQ_over_10.pl
+++ Preprocessing/MAPQ_over_10.pl
@@ -17,7 +17,11 @@ for (@file_list){
 }
 #Extract file name from @file_list directory
 for (@file_list){
-    
+    my @Oname = split(/\//, $_);
+    print("--------\nCutting SAM file named $Oname[-1]...\n--------\n");
+    print("samtools view -q 10 -h -o $_ $Oname[-1]");
+    #`samtools view -q 10 -h -o $directory$_ $_`;
+
 }
 
 #Cut of with samtools

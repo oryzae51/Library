b354be69fa449af2e168f310227d57d543ef0cc7
oryzae51 <oryzae51@gmail.com>
2020-06-18 22:03:29 +0900

    mapq10

--- Preprocessing/MAPQ_over_10.pl
+++ Preprocessing/MAPQ_over_10.pl
@@ -15,10 +15,11 @@ push(@file_list, `find $directory -name '$specific_name'`);
 for (@file_list){
     chomp($_);
 }
+shift(@file_list);
 #Extract file name from @file_list directory
 for (@file_list){
     my @Oname = split(/\//, $_);
-    print("--------\nCutting SAM file named $Oname[-1]...\n--------\n");
+    print("\n--------\nCutting SAM file named $Oname[-1]...\n--------\n");
     print("samtools view -q 10 -h -o $_ $Oname[-1]");
     #`samtools view -q 10 -h -o $directory$_ $_`;
 

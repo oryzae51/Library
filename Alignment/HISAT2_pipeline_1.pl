52981668383dea7977b7c67ee8c012e8cf30c6d8
oryzae51 <oryzae51@gmail.com>
2020-06-23 02:49:40 +0900

    featurecount

--- Alignment/HISAT2_pipeline_1.pl
+++ Alignment/HISAT2_pipeline_1.pl
@@ -26,20 +26,6 @@ for (@file_list){
 for (@file_list){
     print($_);
 }
-# my $i = 0;
-# for (@file_list){
-#     my @Oname = split(/\//, $file_list[$i]);
-#     @Oname = split(/\.f/, $Oname[-1]);
-#     print("Making aligning file named $Oname[0]....\n");
-#     `hisat2 -x $idx_dir -1 $file_list[$i] -2 $file_list[$i+2] -S ~/sequence_data/hisat2_aligned/$Oname[0]`;
-#     if ($i%2==0){
-#         $i+=1;
-#     }
-#     elsif ($i%2!=0){
-#         $i+=3;
-#     }
-#     if ($i >=16){last;}
-# }
 
 for (my $i=0; $i<$#file_list+1; $i+=2){
     my @Oname_1 = split(/\//, $file_list[$i]);

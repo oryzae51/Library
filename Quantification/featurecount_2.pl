dca6d425135ab0cbd5c4fea6ece38f5ff9116a93
oryzae51 <oryzae51@gmail.com>
2020-06-23 22:54:13 +0900

    featurecount 1.10

--- Quantification/featurecount_2.pl
+++ Quantification/featurecount_2.pl
@@ -35,23 +35,22 @@ for (@align_data_list){
 }
 
 #iterate @align_data_list
-foreach(@align_data_list){
-	my @sam_list = ();
-	push(@sam_list, `find "$(pwd)"`);#find absolute path of file
-	shift(@sam_list);
-	@sam_list = sort(@sam_list);
-	for (@sam_list){
-	    chomp($_);
-	}
-	for (@sam_list){
-		print("$_\n");
-	}
 
-	foreach (@sam_list){
-	    my @Oname_1 = split(/\//, $_);
-	    print("Making counting file named $Oname_1[-1]....\n");
-	    print("$_\n");
-	    print("printing command:\nfeatureCounts -T 4 -p -a $gtf_dir -t exon -g gene id -o $out_dir/$Oname_1[-1].txt $_\n\n");
-	    #`featureCounts -T 4 -p -a $gtf_dir -t exon -g gene id -o $_.txt $sam_dir`;
-	}
+my @sam_list = ();
+push(@sam_list, `find "$(pwd)`);#find absolute path of file
+shift(@sam_list);
+@sam_list = sort(@sam_list);
+for (@sam_list){
+    chomp($_);
+}
+for (@sam_list){
+	print("$_\n");
+}
+
+foreach (@sam_list){
+    my @Oname_1 = split(/\//, $_);
+    print("Making counting file named $Oname_1[-1]....\n");
+    print("$_\n");
+    print("printing command:\nfeatureCounts -T 4 -p -a $gtf_dir -t exon -g gene id -o $out_dir/$Oname_1[-1].txt $_\n\n");
+    #`featureCounts -T 4 -p -a $gtf_dir -t exon -g gene id -o $_.txt $sam_dir`;
 }

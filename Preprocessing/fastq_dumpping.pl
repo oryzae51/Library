db531944c73e967c03e8ee2092f3a8689dd2d8b2
oryzae51 <oryzae51@gmail.com>
2020-07-08 20:30:04 +0900

    fastq_dumpping.pl

---
+++ Preprocessing/fastq_dumpping.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+`/media/bm/ETL4TiB/Tools/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --gzip --split-3 /media/bm/ETL4TiB/KHM/rawdata/WT/SRR11179816`
+`/media/bm/ETL4TiB/Tools/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --gzip --split-3 /media/bm/ETL4TiB/KHM/rawdata/WT/SRR11179820`
+`/media/bm/ETL4TiB/Tools/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --gzip --split-3 /media/bm/ETL4TiB/KHM/rawdata/WT/SRR11179824`
+
+`/media/bm/ETL4TiB/Tools/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --gzip --split-3 /media/bm/ETL4TiB/KHM/rawdata/MLL2KO/SRR11179816`
+`/media/bm/ETL4TiB/Tools/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --gzip --split-3 /media/bm/ETL4TiB/KHM/rawdata/MLL2KO/SRR11179820`
+`/media/bm/ETL4TiB/Tools/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --gzip --split-3 /media/bm/ETL4TiB/KHM/rawdata/MLL2KO/SRR11179824`
+
+`/media/bm/ETL4TiB/Tools/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --gzip --split-3 /media/bm/ETL4TiB/KHM/rawdata/SuzKO/SRR11179816`
+`/media/bm/ETL4TiB/Tools/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --gzip --split-3 /media/bm/ETL4TiB/KHM/rawdata/SuzKO/SRR11179820`
+`/media/bm/ETL4TiB/Tools/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --gzip --split-3 /media/bm/ETL4TiB/KHM/rawdata/SuzKO/SRR11179824`
+
+`/media/bm/ETL4TiB/Tools/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --gzip --split-3 /media/bm/ETL4TiB/KHM/rawdata/AzaKO/SRR11179816`
+`/media/bm/ETL4TiB/Tools/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --gzip --split-3 /media/bm/ETL4TiB/KHM/rawdata/AzaKO/SRR11179820`
+`/media/bm/ETL4TiB/Tools/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --gzip --split-3 /media/bm/ETL4TiB/KHM/rawdata/AzaKO/SRR11179824`

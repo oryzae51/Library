51fcfc20b3283f62796bb907d7a0a670f2960008
oryzae51 <oryzae51@gmail.com>
2020-07-08 20:19:29 +0900

    asdf

--- Preprocessing/dumpping.sh
+++ Preprocessing/dumpping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #echo 'Converting 1'
-#echo `~/sratoolkit.2.9.6-1-centos_linux64/bin/fastq-dump --gzip --split-3 ~/sequence_data/raw_seq/SRR486239.2`
+#echo `~/sratoolkit.2.9.6-1-centos_linux64/bin/fastq-dump --gzip --split-3 ~/sequence_data/raw_seq/SRR11179816`
 #echo 'Converting 2'
 #echo `~/sratoolkit.2.9.6-1-centos_linux64/bin/fastq-dump --gzip --split-3 ~/sequence_data/raw_seq/SRR486240.2`
 echo 'Converting 3'

d577cbcdc3c6b6d929dcb0e27a283e8abac3f6eb
oryzae51 <oryzae51@gmail.com>
2020-08-10 16:16:21 +0900

    interation 0810201616

--- RNAseq/DEG/iteration_test.R
+++ RNAseq/DEG/iteration_test.R
@@ -2,7 +2,18 @@ src_dir <- c("/Users/hmkim/data/quant_data/LJK")
 setwd("/Users/hmkim/data/quant_data/LJK")
 src_files <- list.files(src_dir)
 src_files
-
+dirVec <- c()
 for (i in src_files){
-  print(i)
+  direc <- paste(getwd(), "/", i, sep = "")
+  dirVec[length(dirVec) + 1] <- direc
+}
+dirVec
+
+tableList <- list()
+for (i in dirVec){
+  tableList[[length(tableList) + 1]] <- read.table(i, sep = "\t", header = TRUE)
+}
+
+for (i in 1:length(tableList)){
+  tableList[[i]][, 7]
 }

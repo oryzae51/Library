c0312b2c160825ac8797f5a9aa097bb813839ae6
oryzae51 <oryzae51@gmail.com>
2020-08-13 16:16:54 +0900

    deg r codes

--- RNAseq/DEG/edgeR_norep.R
+++ RNAseq/DEG/edgeR_norep.R
@@ -1,3 +1,4 @@
+##edgeR with no replicate
 ##Automate count data.frame 
 #Load data directory
 src_dir <- c("/Users/hmkim/data/quant_data/LJK")
@@ -26,3 +27,16 @@ for (i in 1:length(tableList)){
 rownames(cnts) <- c(tableList[[1]]$Geneid)
 cnts <- cnts[rowSums(cnts > 1) >=length(cnts),]#drop genes with low counts, this is necessary for rld quality
 names(cnts) <- c("S1", "WT") #change col name
+
+##conduct edgeR
+library(edgeR)
+group <- factor(c(2, 1))
+bcv <- 0.1
+y <- DGEList(counts = cnts, group = group)
+et <- exactTest(y, dispersion = bcv^2)
+FDR <- p.adjust(et$table$PValue, method = "BH")
+sum(FDR<0.05)
+et$table <- cbind(et$table, FDR)
+edge_sig <- subset(et$table, FDR<0.05)
+edge_sig <- subset(edge_sig, logFC>1 | logFC< -1)
+sum(edge_sig$FDR<0.01)

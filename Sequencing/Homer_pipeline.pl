98285533dfa75f312a86b4ea5fa206d81da5709e
oryzae51 <oryzae51@gmail.com>
2022-05-04 10:44:37 +0900

    ChIP_read_depth

--- Sequencing/Homer_pipeline.pl
+++ Sequencing/Homer_pipeline.pl
@@ -6,7 +6,7 @@ use warnings;
 print("Write the project directory :\n");
 my $p_directory = <STDIN>;
 chomp($p_directory);
-my $specific_name = "*.duprm.bam"; #specific_name for dup removed bam file
+my $specific_name = "*_duprm.bam"; #specific_name for dup removed bam file
 `mkdir $p_directory/Tagdir_dir`;
 
 #Extract each bam file drictory from bam file dir and save to @file_list
@@ -27,15 +27,11 @@ for (my $i=0; $i<$#file_list+1; $i+=1){
     my @Oname_1 = split(/\//, $file_list[$i]);
     my @Oname_2 = split(/\.fastq-/, $Oname_1[-1]);
     print("Making Tagdir with Homer makeTagDirectory, file named $Oname_2[0]....\n");
+    `mkdir $p_directory/Tagdir_dir/$Oname_2[0]`;
     #print("$file_list[$i]\n");
-    `/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/HOMER/bin/makeTagDirectory \
-$p_directory/Tagdir_dir/$Oname_2[0] \
-$p_directory/aligned/$file_list[$i]` ;
-    print("Making bed file with Homer findPeaks, options -style factor, histone");
-    `/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/HOMER/bin/findPeaks \
-$p_directory/Tagdir_dir/$Oname_2[0] \
--style factor -o auto `;
-    `/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/HOMER/bin/findPeaks \
-$p_directory/Tagdir_dir/$Oname_2[0] \
--style histone -o auto `; 
+    #print("/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/HOMER/bin/makeTagDirectory $p_directory/Tagdir_dir/$Oname_2[0] $file_list[$i]\n");
+    `/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/HOMER/bin/makeTagDirectory $p_directory/Tagdir_dir/$Oname_2[0] $file_list[$i]` ;
+    #print("Making bed file with Homer findPeaks, options -style factor, histone\n");
+    #`/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/HOMER/bin/findPeaks $p_directory/Tagdir_dir/$Oname_2[0] -style factor -o auto`;
+    #`/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/HOMER/bin/findPeaks $p_directory/Tagdir_dir/$Oname_2[0] -style histone -o auto`; 
 }

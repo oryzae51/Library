4fe70d3554d5eb9064c171fb0ae9b5436911dee4
김형민 <kimhyoungmin@gimhyeongmin-ui-Macmini.local>
2022-06-10 20:10:27 +0900

    STAR_V2.pl

--- Sequencing/Alignment/STAR_v2.pl
+++ Sequencing/Alignment/STAR_v2.pl
@@ -6,14 +6,10 @@ use warnings;
 print("Write the project directory :\n");
 my $p_directory = <STDIN>;
 
-my $indx = <STDIN>;
-chomp($indx); #Index file directory and basename
 print("Write the basename of files :\n");
 my $specific_name = "*.fastq.gz"; #specific_name for fastq file
 chomp($specific_name);
-print("Write the file directory for SAM output :\n");
-my $hit_file = <STDIN>;
-chomp($hit_file); #file directory for saveM output
+
 print("gzip file: 0, non zip file: 1\n");
 my $zipcon = <STDIN>;
 chomp($zipcon);
@@ -22,6 +18,7 @@ my $paircon = <STDIN>;
 chomp($paircon);
 my $con = $paircon.$zipcon;
 print("$con\n");
+`mkdir $p_directory/aligned`;
 
 #Extract each fastq file drictory from $fastq_dir and save to @file_list
 #my $file = `find $directory -name "*.fastq"`;
@@ -46,8 +43,8 @@ if ($con == 00){
     my @Oname_2 = split(/_/, $Oname_1[-1]);
     print("Making alignment file named $Oname_2[0]....\n");
     print("$file_list[$i]\n");
-	print("printing command:\n\n/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir $indx --readFilesCommand zcat --readFilesIn $file_list[$i] --outFileNamePrefix $hit_file/$Oname_2[0]\n");
-    `/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir $indx --readFilesCommand zcat --readFilesIn $file_list[$i] --outFileNamePrefix $hit_file/$Oname_2[0]`;
+	print("printing command:\n\n/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir /media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/KHM/align_data/STARindx/hg38 --readFilesCommand zcat --readFilesIn $file_list[$i] --outFileNamePrefix $p_directory/aligned/$Oname_2[0]\n");
+    `/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir /media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/KHM/align_data/STARindx/hg38 --readFilesCommand zcat --readFilesIn $file_list[$i] --outFileNamePrefix $p_directory/aligned/$Oname_2[0]`;
 	}
 } elsif ($con == 01){
 	#single, non-zip
@@ -56,8 +53,8 @@ if ($con == 00){
     my @Oname_2 = split(/_/, $Oname_1[-1]);
     print("Making alignment file named $Oname_2[0]....\n");
     print("$file_list[$i]\n");
-	print("printing command:\n\n/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir $indx --readFilesIn $file_list[$i] --outFileNamePrefix $hit_file/$Oname_2[0]\n");
-    `/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir $indx --readFilesIn $file_list[$i] --outFileNamePrefix $hit_file/$Oname_2[0]`;
+	print("printing command:\n\n/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir /media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/KHM/align_data/STARindx/hg38 --readFilesIn $file_list[$i] --outFileNamePrefix $p_directory/aligned/$Oname_2[0]\n");
+    `/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir /media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/KHM/align_data/STARindx/hg38 --readFilesIn $file_list[$i] --outFileNamePrefix $p_directory/aligned/$Oname_2[0]`;
 	}
 } elsif ($con == 10){
 	#pair, gzip
@@ -67,8 +64,8 @@ if ($con == 00){
     print("Making alignment file named $Oname_2[0]....\n");
     print("$file_list[$i]\n");
     print("$file_list[$i+1]\n");
-	print("printing command:\n\n/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir $indx --readFilesCommand zcat --readFilesIn $file_list[$i] $file_list[$i+1] --outFileNamePrefix $hit_file/$Oname_2[0]\n");
-    `/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir $indx --readFilesCommand zcat --readFilesIn $file_list[$i] $file_list[$i+1] --outFileNamePrefix $hit_file/$Oname_2[0]`;
+	print("printing command:\n\n/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir /media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/KHM/align_data/STARindx/hg38 --readFilesCommand zcat --readFilesIn $file_list[$i] $file_list[$i+1] --outFileNamePrefix $p_directory/aligned/$Oname_2[0]\n");
+    `/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir /media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/KHM/align_data/STARindx/hg38 --readFilesCommand zcat --readFilesIn $file_list[$i] $file_list[$i+1] --outFileNamePrefix $p_directory/aligned/$Oname_2[0]`;
 	}
 } elsif ($con == 11){
 	#pair, non-zip
@@ -78,7 +75,7 @@ if ($con == 00){
     print("Making alignment file named $Oname_2[0]....\n");
     print("$file_list[$i]\n");
     print("$file_list[$i+1]\n");
-	print("printing command:\n\n/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir $indx --readFilesIn $file_list[$i] $file_list[$i+1] --outFileNamePrefix $hit_file/$Oname_2[0]\n");
-    `/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir $indx --readFilesIn $file_list[$i] $file_list[$i+1] --outFileNamePrefix $hit_file/$Oname_2[0]`;
+	print("printing command:\n\n/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir /media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/KHM/align_data/STARindx/hg38 --readFilesIn $file_list[$i] $file_list[$i+1] --outFileNamePrefix $p_directory/aligned/$Oname_2[0]\n");
+    `/media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/Tools/STAR-2.7.5a/bin/Linux_x86_64/STAR --runThreadN 14 --genomeDir /media/bm/790240e4-2887-451f-ad02-1b19c4b4e120/KHM/align_data/STARindx/hg38 --readFilesIn $file_list[$i] $file_list[$i+1] --outFileNamePrefix $p_directory/aligned/$Oname_2[0]`;
 	}
 }

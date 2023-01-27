5439132462724e41cd479bf9a2af425cf0c7ac0e
oryzae51 <oryzae51@gmail.com>
2023-01-27 15:48:41 +0900

    skewer_pipe

---
+++ Sequencing/Alignment/skewer.pl
@@ -0,0 +1,29 @@
+#usr/bin/perl
+use strict;
+use warnings;
+
+#Project directory
+print("Write the project directory :\n");
+my $p_directory = <STDIN>;
+chomp($p_directory);
+my $specific_name = "*.fastq.gz"; #specific_name for fastq file
+`mkdir $p_directory/aligned`;
+
+#Extract each fastq file drictory from $fastq_dir and save to @file_list
+my @file_list = ();
+push(@file_list, `find $p_directory/trimmed -name '$specific_name'`);
+@file_list = sort(@file_list); #sort in suffix
+#check if the @file_list is made correctly
+for (@file_list){
+    chomp($_);
+}
+for (@file_list){
+    print("$_\n");
+}
+
+for (my $i=0; $i<$#file_list+1; $i+=2){
+    my @Oname_1 = split(/\//, $file_list[$i]);
+    my @Oname_2 = split(/\.fastq\./, $Oname_1[-1]);
+    print("Making alignment file named $Oname_2[0]....\n");
+    `skewer -t 4 -z -x AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -y AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT $file_list[$i] $file_list[$i+1]`;
+}
